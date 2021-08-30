c5459460abb79f66d8dc16f4f198959aac8020cf
Tolerance <zpgy77@gmail.com>
2021-08-30 22:54:05 +0900

    MVC and template engine

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,11 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         // resources/template에 있는 html파일 이름과 똑같음
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
