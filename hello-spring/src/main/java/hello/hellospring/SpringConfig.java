2a5046ebcf66ef768a3647137351d775b7bf5cf5
Tolerance <zpgy77@gmail.com>
2021-09-07 07:32:03 +0900

    Pure JDBC

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig  {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // 스프링 빈에 등록하라는 걸로 인
     @Bean
@@ -15,8 +25,10 @@ public class SpringConfig  {
         return new MemberService(memberRepository());
     }
 
+    // 디펜던시 인젝션, 객체지향 프로그래밍. 기존의 코드는 하나도 건드리지않고, 어플리케이셔은을 조립하는 컨피그(어셈블리)코드만 수정했음
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
