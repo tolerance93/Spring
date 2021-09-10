70819ec31d8fc1722daa9d2a86129ec985cad7af
Tolerance <zpgy77@gmail.com>
2021-09-10 10:59:32 +0900

    JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,23 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig  {
 
-    private DataSource dataSource;
+    EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     // 스프링 빈에 등록하라는 걸로 인
@@ -28,8 +29,9 @@ public class SpringConfig  {
     // 디펜던시 인젝션, 객체지향 프로그래밍. 기존의 코드는 하나도 건드리지않고, 어플리케이셔은을 조립하는 컨피그(어셈블리)코드만 수정했음
     @Bean
     public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
         // return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,12 +5,13 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 // command + shift + T: 테스트케이스 자동생성
-
+@Transactional // jpa는 데이터변경이 transactional안에서 실행이 되어야 함
 public class MemberService {
 
     private final MemberRepository memberRepository;
