b6f3b0d6578bc4a94928bbbc1e7425bed8d993a6
Tolerance <zpgy77@gmail.com>
2021-09-16 22:28:13 +0900

    Project Setup

---
+++ core/src/test/java/store/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package store.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
