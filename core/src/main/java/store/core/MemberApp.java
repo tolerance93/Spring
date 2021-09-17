9817d51688b4b8a65e925fea6f2fd47ac68e04ef
Tolerance <zpgy77@gmail.com>
2021-09-17 22:25:35 +0900

    Run member domain and test

---
+++ core/src/main/java/store/core/MemberApp.java
@@ -0,0 +1,21 @@
+package store.core;
+
+import store.core.member.Grade;
+import store.core.member.Member;
+import store.core.member.MemberService;
+import store.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        // soutv
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+}
--- core/src/main/java/store/core/member/MemberServiceImpl.java
+++ core/src/main/java/store/core/member/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package store.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // command + shift + enter: 자동완성 + ;
+    // 추상화에도 구체화에도 의존함. 문제!!!!! DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
