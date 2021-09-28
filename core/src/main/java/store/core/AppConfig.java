b0ab6c63965a47b920c4c03aa79296295580456e
Tolerance <zpgy77@gmail.com>
2021-09-28 23:10:49 +0900

    Refactor Appconfig

--- core/src/main/java/store/core/AppConfig.java
+++ core/src/main/java/store/core/AppConfig.java
@@ -1,6 +1,8 @@
 package store.core;
 
+import store.core.discount.DiscountPolicy;
 import store.core.discount.FixDiscountPolicy;
+import store.core.member.MemberRepository;
 import store.core.member.MemberService;
 import store.core.member.MemberServiceImpl;
 import store.core.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import store.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // command + option + M: extract method
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
