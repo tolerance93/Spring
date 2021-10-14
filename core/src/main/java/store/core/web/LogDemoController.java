d29588d1e7bd1a5554e1a5973cdfd6f80546729a
Tolerance <zpgy77@gmail.com>
2021-10-14 21:45:07 +0900

    scope and provider

--- core/src/main/java/store/core/web/LogDemoController.java
+++ core/src/main/java/store/core/web/LogDemoController.java
@@ -1,6 +1,7 @@
 package store.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
--- core/src/main/java/store/core/web/LogDemoService.java
+++ core/src/main/java/store/core/web/LogDemoService.java
@@ -1,6 +1,7 @@
 package store.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import store.core.common.MyLogger;
 
@@ -8,9 +9,10 @@ import store.core.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
